7267ce474b2378a1334cf31384d6b8f1d88486e0
Shanto2378 <hasibul.shanto1@northsouth.edu>
2024-03-08 03:45:19 +0600

    code cleaned up, next target to take user input

--- network_scanner.py
+++ network_scanner.py
@@ -7,11 +7,19 @@ def scan(ip):
     arp_request = scapy.ARP(pdst = ip) # creating an ARP request object
     broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff") # creating an Ether object
     arp_request_broadcast = broadcast/arp_request # combining the two objects
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout = 1) # sending the request and storing the answered and unanswered requests
-    print(answered.summary()) # printing the summary of the answered requests
-    
+    answered_list = scapy.srp(arp_request_broadcast, timeout = 1, verbose = False)[0] # sending the request and storing the answered and unanswered requests
 
+    client_list = [] # creating an empty list to store the IP and MAC addresses of the answered requests
+    print("-----------------------------------------") # printing the header
+    for element in answered_list:
+        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc} # creating a dictionary to store the IP and MAC addresses of the answered requests
+        client_list.append(client_dict) # appending the dictionary to the list
+    return client_list # returning the list
 
-
-scan("192.168.225.1/24") # calling the function with the ip address of the network
-
+def print_result(results_list):
+    print("IP \t\t\t MAC Address\n-----------------------------------------") # printing the header
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"]) # printing the IP and MAC addresses of the answered requests
+ 
+scan_result = scan("10.0.2.1/24") # calling the function with the ip address of the network which is "route -n" 
+print_result(scan_result)
